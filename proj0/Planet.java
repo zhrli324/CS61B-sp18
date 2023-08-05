21144dd5981cd23ab04aa15c9268dabeec00a4e6
zhrli <zhrli@outlook.com>
2023-08-06 00:40:50 +0800

    proj0 revise_1

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -2,7 +2,7 @@ public class Planet
 {
 	public double xxPos, yyPos, xxVel, yyVel, mass;
 	public String imgFileName;
-	public static double G = 6.67e-11;
+	private static double G = 6.67e-11;
 	
 	public Planet(double xP, double yP, double xV,
 				  double yV, double m, String img)
