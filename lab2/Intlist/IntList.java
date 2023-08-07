f77b3610cbe41e407eb0de46ec66431380f89ba4
zhrli <zhrli@outlook.com>
2023-08-07 12:58:47 +0800

    lab2 completed

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,11 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null) return B;
+        IntList p = A;
+        while(p.rest != null) p = p.rest;
+        p.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +95,8 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null) return B;
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
