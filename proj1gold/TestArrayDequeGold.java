ca5dc8ec252939fd3b520101d09443014c18ddbc
zhrli <zhrli@outlook.com>
2023-08-17 00:58:17 +0800

    completed proj1gold v3

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -8,7 +8,55 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> correctArrayDeque = new ArrayDequeSolution<>();
         String log = "";
 
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 1000; i++) {
+            if (testArrayDeque.size() == 0) {
+                int addNum = StdRandom.uniform(1000);
+                double half = StdRandom.uniform();
+
+                if (half < 0.5) {
+                    testArrayDeque.addFirst(addNum);
+                    correctArrayDeque.addFirst(addNum);
+                    log = log + "addFirst(" + addNum + ")\n";
+                } else {
+                    testArrayDeque.addLast(addNum);
+                    correctArrayDeque.addLast(addNum);
+                    log = log + "addLast(" + addNum + ")\n";
+                }
+            } else {
+                int dice = StdRandom.uniform(4);
+                int addNum = StdRandom.uniform(1000);
+                Integer testRemove;
+                Integer correctRemove;
+                Integer num1 = 1;
+                Integer num2 = 1;
+
+                switch (dice) {
+                    case 0:
+                        testArrayDeque.addFirst(addNum);
+                        correctArrayDeque.addFirst(addNum);
+                        log = log + "addFirst(" + addNum + ")\n";
+                        break;
+                    case 1:
+                        testArrayDeque.addLast(addNum);
+                        correctArrayDeque.addLast(addNum);
+                        log = log + "addLast(" + addNum + ")\n";
+                        break;
+                    case 2:
+                        num1 = testArrayDeque.removeFirst();
+                        num2 = correctArrayDeque.removeFirst();
+                        log = log + "removeFirst(): " + num1 + "\n";
+                        break;
+                    case 3:
+                        num1 = testArrayDeque.removeLast();
+                        num2 = correctArrayDeque.removeLast();
+                        log = log + "removeLast(): " + num1 + "\n";
+                }
+
+                assertEquals(log, num1, num2);
+            }
+        }
+
+/*        for (int i = 0; i < 30; i++) {
             double numBetweenZeroToOne = StdRandom.uniform();
 
             if (numBetweenZeroToOne < 0.5) {
@@ -23,15 +71,7 @@ public class TestArrayDequeGold {
                 log = log + "addLast(" + ran + ")\n";
             }
         }
-/*
-        for (int i = 0; i < 50; i++) {
-            int intBetweenZeroTo = StdRandom.uniform(100);
-            assertEquals("addFirst() or addLast(): "
-                            + testArrayDeque.get(intBetweenZeroTo),
-                            correctArrayDeque.get(intBetweenZeroTo),
-                            testArrayDeque.get(intBetweenZeroTo));
-        }
-*/
+
         for (int i = 0; i < 30; i++) {
             double numBetweenZeroToOne = StdRandom.uniform();
 
@@ -47,7 +87,7 @@ public class TestArrayDequeGold {
                 assertEquals(log, correctRes, testRes);
             }
         }
-
+*/
     }
 }
 /*
